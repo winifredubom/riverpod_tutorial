6bf52d837e5194c202e9731a3928bb829c71f46f
Winifred <winifredubom@gmail.com>
2024-07-16 14:18:53 +0100

    video 4 commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 //i learnt about:
-//Riverpod, Its a state management that has similiarities like that of Provider package
-// also learnt about the widget ProviderScope which has a job of storing
-// all the state of providers that would be created in this application and it makes sure
-// that the states are tested and maintained.
+//ConsumerWidgets which are widgets of provider help to access and
+// react to the changes made in the state.
 
-// this is a variable that is defined as a Provider type String
+//watch() listens to the providers inorder to be know the changes made to the providers state so as to rebuild it
 final nameProvider = Provider<String>((ref) {
 return 'Hello Joy';
 });
@@ -26,53 +24,25 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
 
+class MyHomePage extends ConsumerWidget {
+  const MyHomePage({super.key});
+ 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final name = ref.watch(nameProvider);
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: const Text('RIverpod Provider'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        child: Text(name),
+      ),// This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
