dddcb5f99b11a225979d46c23c3b1d7bf0c664ef
Winifred <winifredubom@gmail.com>
2024-07-19 09:15:18 +0100

    video 8 commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_practice/counter_demo.dart';
 
 //i learnt about: 
-//The second type of provider which is StateProvider
-//This type of provider is used to store simple mutable object that cant be changed like:
-//String, enum, boolean. But it cant be used to store complex objects
-// ref. watch is used to observe the provider state inside the build method and when the provider value changes it rebuilds the widget
-// ref.read is used to read the provider value once 
-// 
+// The third type of provider which is StateNotifierProvider
+// It is a provider that is used to listen to and expose a StateNotifier, in which working hand in hand, they manage state which may change in reaction to a user reaction
+// It is used for centralizing Business logic in a single place and improving maintainability of the code over time
+//  
 
 
-final counterProvider = StateProvider<int>(
-  (ref) => 0
-  );
-  
+final counterProvider = StateNotifierProvider<CounterDemo, int>((ref) => CounterDemo());
 void main() {
   runApp(const ProviderScope(child: MyApp()));
 }
@@ -40,31 +36,21 @@ class Main extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final count = ref.watch(counterProvider);
+    final counter = ref.watch(counterProvider);
 
-// ref.listen is to listen to the changes made
-    ref.listen(counterProvider, ((previous, next) {
-      if (next == 5)
-      {
-        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('The Value is $next')));
-      }
-    }));
     return Scaffold(
       appBar: AppBar(
         title: const Text('State Provider'),
         actions: [
           IconButton(
             onPressed:(){
-              ref.invalidate(counterProvider);
-              // or
-             // ref.refresh(counterProvider);
                           } ,
             icon: const Icon(Icons.refresh))
         ],
       ),
-      body:  Center(
+      body:   Center(
         child: Text(
-          count.toString(),
+          '$counter',
            style: const TextStyle(
             fontSize: 20
           ),
@@ -72,9 +58,7 @@ class Main extends ConsumerWidget {
       ),
       floatingActionButton: 
       FloatingActionButton(onPressed: (){
-        ref.read(counterProvider.notifier).state++;
-        // or
-       // ref.read(counterProvider.notifier).update((state) => state + 1); 
+        ref.read(counterProvider.notifier).increment();
       },
       child: const Icon(Icons.add),),
     );
