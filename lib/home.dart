6de4e3c85c4cb292d2ef0b0509e9c94c9c1e99a8
Winifred <winifredubom@gmail.com>
2024-07-21 22:11:20 +0100

    video 12 commit

--- lib/home.dart
+++ lib/home.dart
@@ -14,6 +14,7 @@ class Home extends StatelessWidget {
           (onPressed: (){
             context.push('/counter');
           }, child: const Text('Counter')) ,),
+          
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,20 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
 import 'package:riverpod_practice/counter.dart';
 import 'package:riverpod_practice/home.dart';
 
 
 //i learnt about: 
-// AutoDispose Modifier in Riverpod 
-// AutoDispose is used to destroy the state of a provider when it is no longer used.
-// it is also used to reset the state when a user leaves the screen and re-enters it
-// GoRouter dependency was used here, its a declarative routing package used for making navigations in flutter apps simpler
-// keepAlive Function is used to tell Riverpod that the state of the provider should be preserved even if no longer listened to
-// the Timer was made use of to dispose the providers state after a given duration
+// Family Modifier in Riverpod 
+// They are used to pass values to a provider
+// The only limitataion is that we can pass only a single value
 
-
+final nameProvider = Provider.family<String, String>(
+  (ref, name) {
+    return 'Hello $name';
+  }
+);
 void main() {
   runApp(const ProviderScope(child: MyApp()));
 }
@@ -25,20 +25,41 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
+    return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      routerConfig: _router
+      home: const Main()
     );
   }
 }
 
 
-final GoRouter _router  = GoRouter(routes:[
-  GoRoute(path: '/', builder: (context, state) => const Home()),
-  GoRoute(path: '/counter', builder: (context, state) => const Counter()),
-]);
+class Main extends ConsumerStatefulWidget {
+  const Main({super.key});
+
+  @override
+  _MainState createState() => _MainState();
+}
+
+class _MainState extends ConsumerState<Main> {
+  @override
+  Widget build(BuildContext context) {
+    final name = ref.watch(nameProvider('Jiggy Joy'));
+
+    return  Scaffold(
+      appBar: AppBar(
+        title: const Text('Family Modifier'),
+      ),
+      body: Center(
+        child: Text(name,
+        style: 
+       const TextStyle(
+          fontSize: 20
+        )),),
+    );
+  }
+}
 
