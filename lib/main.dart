06ab4561d668fe31d04aab621c1816ad7b0acd66
Winifred <winifredubom@gmail.com>
2024-07-21 23:12:10 +0100

    video 14 commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,63 +3,55 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_practice/user.dart';
 
-
-
 //i learnt about: 
-// Family Modifier(multiple values) in Riverpod 
-// They are used to pass values to a provider
+// how to change theme from Light to Dark and vice versa
 
-final nameProvider = Provider.family<String, User>(
-  (ref, user) {
-    return 'Name: ${user.name} and Address: ${user.address} ';
-  }
-);
+
+final themeprovider  = StateProvider<bool>((ref) => true);
 void main() {
   runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+final isLightTheme = ref.watch(themeprovider);
+
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Theme',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme:  isLightTheme? ThemeData.light(): ThemeData.dark(),
       home: const Main()
     );
   }
 }
 
-
-class Main extends ConsumerStatefulWidget {
+class Main extends ConsumerWidget {
   const Main({super.key});
 
   @override
-  _MainState createState() => _MainState();
-}
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isLightTheme = ref.watch(themeprovider);
 
-class _MainState extends ConsumerState<Main> {
-  @override
-  Widget build(BuildContext context) {
-    final name = ref.watch(
-      nameProvider(const User('Joy', 'Lagos')));
 
     return  Scaffold(
       appBar: AppBar(
-        title: const Text('Family Modifier'),
+        title: const Text(
+          'Theme'
+        ),
       ),
       body: Center(
-        child: Text(name,
-        style: 
-       const TextStyle(
-          fontSize: 20
-        )),),
+        child: Switch(
+          activeColor: Colors.blue,
+          value: isLightTheme , onChanged: (value){
+            ref.read(themeprovider.notifier).state = value;
+          }),
+      ),
     );
   }
 }
+
 
