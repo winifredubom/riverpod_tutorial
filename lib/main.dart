5a959e513aa0fbc25b848f27c6ef3555ffa3d1b7
Winifred <winifredubom@gmail.com>
2024-07-21 22:14:21 +0100

    video 12 commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:riverpod_practice/counter.dart';
-import 'package:riverpod_practice/home.dart';
+
 
 
 //i learnt about: 
